35cd277b6b21e1fe27d8e1b26d2ca2d8bf671b1c
dannyboy <jdstevens03@gmail.com>
2024-02-08 21:01:34 -0600

    first commit to package

--- src/DanPackageTest.jl
+++ src/DanPackageTest.jl
@@ -1,5 +1,7 @@
 module DanPackageTest
 
 # Write your package code here.
+export greet_your_package_name
+include("functions.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "DanPackageTest.jl" begin
     # Write your tests here.
+    @test DanPackageTest.greet_your_package_name() == "Hello DanPackageTest!"
+    @test DanPackageTest.greet_your_package_name() != "Hello world!"
 end
